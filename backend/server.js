2d886013b30bea5d073530c055fb392e1f6cf4d2
Parth0124 <abhangparth@gmail.com>
2024-10-28 01:07:11 +0530

    created frontend folder and added nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ const express = require('express')
 
 const app = express()
 
-app.listen(4000, console.log('Server started on port 4000!'))
+app.listen(4000, console.log('Server started on port 4000'))
